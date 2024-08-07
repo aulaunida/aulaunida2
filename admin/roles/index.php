34a564fc010b1890651dfd9e124b79c412128c2b
rodriguezcl <cristianleonardorodriguez1991@gmail.com>
2024-08-07 20:42:36 -0300

    Checking repositorio notebook enova

--- admin/roles/index.php
+++ admin/roles/index.php
@@ -1,8 +1,8 @@
 <?php
-include ('../../app/config.php');
-include ('../../admin/layout/parte1.php');
+include('../../app/config.php');
+include('../../admin/layout/parte1.php');
 
-include ('../../app/controllers/roles/listado_de_roles.php');
+include('../../app/controllers/roles/listado_de_roles.php');
 
 ?>
 
@@ -28,55 +28,61 @@ include ('../../app/controllers/roles/listado_de_roles.php');
                         <div class="card-body">
                             <table id="example1" class="table table-striped table-bordered table-hover table-sm">
                                 <thead>
-                                <tr>
-                                    <th><center>Nro</center></th>
-                                    <th><center>Nombre del rol</center></th>
-                                    <th><center>Acciones</center></th>
-                                </tr>
+                                    <tr>
+                                        <th>
+                                            <center>Nro</center>
+                                        </th>
+                                        <th>
+                                            <center>Nombre del rol</center>
+                                        </th>
+                                        <th>
+                                            <center>Acciones</center>
+                                        </th>
+                                    </tr>
                                 </thead>
                                 <tbody>
-                                <?php
-                                $contador_rol = 0;
-                                foreach ($roles as $role){
-                                    $id_rol = $role['id_rol'];
-                                    $contador_rol = $contador_rol +1; ?>
-                                    <tr>
-                                        <td style="text-align: center"><?=$contador_rol;?></td>
-                                        <td><?=$role['nombre_rol'];?></td>
-                                        <td style="text-align: center">
-                                            <div class="btn-group" role="group" aria-label="Basic example">
-                                                <a href="show.php?id=<?=$id_rol;?>" type="button" class="btn btn-info btn-sm"><i class="bi bi-eye"></i></a>
-                                                <a href="edit.php?id=<?=$id_rol;?>" type="button" class="btn btn-success btn-sm"><i class="bi bi-pencil"></i></a>
-                                                <form action="<?=APP_URL;?>/app/controllers/roles/delete.php" onclick="preguntar<?=$id_rol;?>(event)" method="post" id="miFormulario<?=$id_rol;?>">
-                                                    <input type="text" name="id_rol" value="<?=$id_rol;?>" hidden>
-                                                    <button type="submit" class="btn btn-danger btn-sm" style="border-radius: 0px 5px 5px 0px"><i class="bi bi-trash"></i></button>
-                                                </form>
-                                                <script>
-                                                    function preguntar<?=$id_rol;?>(event) {
-                                                        event.preventDefault();
-                                                        Swal.fire({
-                                                            title: 'Eliminar registro',
-                                                            text: '¿Desea eliminar este registro?',
-                                                            icon: 'question',
-                                                            showDenyButton: true,
-                                                            confirmButtonText: 'Eliminar',
-                                                            confirmButtonColor: '#a5161d',
-                                                            denyButtonColor: '#270a0a',
-                                                            denyButtonText: 'Cancelar',
-                                                        }).then((result) => {
-                                                            if (result.isConfirmed) {
-                                                                var form = $('#miFormulario<?=$id_rol;?>');
-                                                                form.submit();
-                                                            }
-                                                        });
-                                                    }
-                                                </script>
-                                            </div>
-                                        </td>
-                                    </tr>
                                     <?php
-                                }
-                                ?>
+                                    $contador_rol = 0;
+                                    foreach ($roles as $role) {
+                                        $id_rol = $role['id_rol'];
+                                        $contador_rol = $contador_rol + 1; ?>
+                                        <tr>
+                                            <td style="text-align: center"><?= $contador_rol; ?></td>
+                                            <td><?= $role['nombre_rol']; ?></td>
+                                            <td style="text-align: center">
+                                                <div class="btn-group" role="group" aria-label="Basic example">
+                                                    <a href="show.php?id=<?= $id_rol; ?>" type="button" class="btn btn-info btn-sm"><i class="bi bi-eye"></i></a>
+                                                    <a href="edit.php?id=<?= $id_rol; ?>" type="button" class="btn btn-success btn-sm"><i class="bi bi-pencil"></i></a>
+                                                    <form action="<?= APP_URL; ?>/app/controllers/roles/delete.php" onclick="preguntar<?= $id_rol; ?>(event)" method="post" id="miFormulario<?= $id_rol; ?>">
+                                                        <input type="text" name="id_rol" value="<?= $id_rol; ?>" hidden>
+                                                        <button type="submit" class="btn btn-danger btn-sm" style="border-radius: 0px 5px 5px 0px"><i class="bi bi-trash"></i></button>
+                                                    </form>
+                                                    <script>
+                                                        function preguntar<?= $id_rol; ?>(event) {
+                                                            event.preventDefault();
+                                                            Swal.fire({
+                                                                title: 'Eliminar registro',
+                                                                text: '¿Desea eliminar este registro?',
+                                                                icon: 'question',
+                                                                showDenyButton: true,
+                                                                confirmButtonText: 'Eliminar',
+                                                                confirmButtonColor: '#a5161d',
+                                                                denyButtonColor: '#270a0a',
+                                                                denyButtonText: 'Cancelar',
+                                                            }).then((result) => {
+                                                                if (result.isConfirmed) {
+                                                                    var form = $('#miFormulario<?= $id_rol; ?>');
+                                                                    form.submit();
+                                                                }
+                                                            });
+                                                        }
+                                                    </script>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    <?php
+                                    }
+                                    ?>
                                 </tbody>
                             </table>
                         </div>
@@ -92,13 +98,13 @@ include ('../../app/controllers/roles/listado_de_roles.php');
 
 <?php
 
-include ('../../admin/layout/parte2.php');
-include ('../../layout/mensajes.php');
+include('../../admin/layout/parte2.php');
+include('../../layout/mensajes.php');
 
 ?>
 
 <script>
-    $(function () {
+    $(function() {
         $("#example1").DataTable({
             "pageLength": 5,
             "language": {
@@ -120,26 +126,27 @@ include ('../../layout/mensajes.php');
                     "previous": "Anterior"
                 }
             },
-            "responsive": true, "lengthChange": true, "autoWidth": false,
+            "responsive": true,
+            "lengthChange": true,
+            "autoWidth": false,
             buttons: [{
-                extend: 'collection',
-                text: 'Reportes',
-                orientation: 'landscape',
-                buttons: [{
-                    text: 'Copiar',
-                    extend: 'copy',
-                }, {
-                    extend: 'pdf'
-                },{
-                    extend: 'csv'
-                },{
-                    extend: 'excel'
-                },{
-                    text: 'Imprimir',
-                    extend: 'print'
-                }
-                ]
-            },
+                    extend: 'collection',
+                    text: 'Reportes',
+                    orientation: 'landscape',
+                    buttons: [{
+                        text: 'Copiar',
+                        extend: 'copy',
+                    }, {
+                        extend: 'pdf'
+                    }, {
+                        extend: 'csv'
+                    }, {
+                        extend: 'excel'
+                    }, {
+                        text: 'Imprimir',
+                        extend: 'print'
+                    }]
+                },
                 {
                     extend: 'colvis',
                     text: 'Visor de columnas',
